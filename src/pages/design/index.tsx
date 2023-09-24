2ddb973c3e3e1e6410cab58a37f549ff64e07816
grantmbb@live.com <grantmbb@live.com>
2023-09-24 15:50:00 -0400

    Initial

---
+++ src/pages/design/index.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+
+const DesignIndex:NextPage = () => {
+    return <h1>Design Index</h1>;
+}
+
+export default DesignIndex
---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Iamge from 'next/image'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+    return (
+        <>
+        <Head>
+        </Head>
+        <main><h1 className='text-3xl font-bold underline'>Hello World</h1></main>
+        </>
+    )
+}
