10351f496afd73eeaed0d2f8b15e42698a8c9969
grantmbb@live.com <grantmbb@live.com>
2023-09-24 16:20:27 -0400

    Parallax

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import type { AppProps } from 'next/app'
 import '@/styles/globals.css'
+import { ParallaxProvider } from 'react-scroll-parallax'
  
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ParallaxProvider scrollAxis='vertical'>
+        <Component {...pageProps} />
+    </ParallaxProvider>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,13 @@ import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
+
 export default function Home() {
     return (
         <>
         <Head>
         </Head>
         <main><h1 className='text-3xl font-bold underline'>Hello World</h1></main>
-        </>
+      </>
     )
 }
